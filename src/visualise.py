7ccb019a58b034c28f6c939757d5a69c7a327386
Ttheo22 <theofsx@gmail.com>
2026-05-02 16:18:36 +0000

    add main

--- src/visualise.py
+++ src/visualise.py
@@ -10,9 +10,9 @@ inflation_df = df[df['indicator_name'] == 'inflation_rate']
 def plot_inflation():
     inflation_df = df[df['indicator_name'] == 'inflation_rate']
 
-    plt.figure(figsize=(12,5))
+    plt.figure(figsize=(10,5))
     plt.plot(inflation_df['year'], inflation_df['value'], marker='o',color='red')
-    plt.title('Ghana Inflation Rate (2000-2023)')
+    plt.title('Ghana Inflation Rate (2000-2025)')
     plt.xlabel('Year')
     plt.ylabel('Inflation %')
     plt.grid(True)
@@ -23,7 +23,7 @@ def plot_inflation():
 def plot_all_indicators():
     indicators = df['indicator_name'].unique()
     
-    fig, axes = plt.subplots(len(indicators), 1, figsize=(12, 5* len(indicators)))
+    fig, axes = plt.subplots(len(indicators), 1, figsize=(10,5* len(indicators)))
     
     for i, indicator in enumerate(indicators):
         indicator_df = df[df['indicator_name'] == indicator]
